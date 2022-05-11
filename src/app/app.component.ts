bbbc99596306ca4907a85a8c01614845fa8e86d9
Anh Quan Nguyen <Anh.Nguyen.extern@dvag.com>
2022-05-11 14:39:17 +0200

    Add todolist, todoitem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {TodoService} from "./services/todo.service";
+import {Todo} from "./model/todo";
+import {User} from "./model/user";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  providers: [TodoService]
 })
 export class AppComponent {
   title = 'todos-app';
+
+  appAuthor = new User('Anh Quan', 'Nguyen');
+  newTodo: Todo = new Todo();
+
+  // @devnote: add 'private' to quickly declare a private class property. This property is automatically injected to constructor
+  constructor(private todoService: TodoService) {
+    console.log('AppComponent created: todoService', todoService);
+  }
+
+  // @devnote using 'get' to declare a getter => This will be called by 'appComponentInstance.todoList'
+  get todoList() {
+    console.log('AppComponent.todoList: getter is called')
+    return this.todoService.getAllTodos();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { UserComponent } from './components/user/user.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    AppHeaderComponent,
+    UserComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
